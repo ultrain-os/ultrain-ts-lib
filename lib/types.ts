0dd047528591369ee2744e85ef28fe7c014ace16
Palmer <pengliao@live.cn>
2018-05-11 09:44:01 +0800

    add types

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,20 @@
 /**
  * @author fanliangqin@ultrain.io
  */
+
+/**
+* @defgroup type Buildin types
+*/
 declare type Name = u64;
+declare type account_name = i64;
+declare type permission_name = u64;
+declare type token_name = u64;
+declare type table_name = u64;
+declare type time = u32;
+declare type scope_name = u64;
+declare type action_name = u64;
+declare type region_id = u16;
+
+declare type asset_symbol = u64;
+declare type share_type = i16;
+declare type weight_type = u16;
