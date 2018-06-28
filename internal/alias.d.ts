ad3aa679888af695060721205339a0c2f6ccc5d6
Palmer <pengliao@live.cn>
2018-06-28 14:12:41 +0800

    add id_type alias
    Signed-off-by: Palmer <pengliao@live.cn>

--- internal/alias.d.ts
+++ internal/alias.d.ts
@@ -4,12 +4,13 @@
 
 declare type token_name = u64;
 declare type region_id = u16;
+declare type id_type = u64;
 
 declare type asset_symbol = u64;
 declare type share_type = i16;
 
 declare type symbol_name = u64;
-declare type account_name = i64;
+declare type account_name = u64;
 declare type permission_name = u64;
 declare type table_name = u64;
 declare type time = u32;
