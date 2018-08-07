10251d0f53fc6d157d2cf764011ff4ff73bf137c
fanliangqin <fanliangqin@ultrain.io>
2018-08-07 17:14:16 +0800

    isConcreteWasmType exception

--- example/dragon/fightcore.ts
+++ example/dragon/fightcore.ts
@@ -6,6 +6,7 @@ import { ultrain_assert } from "../../src/utils";
 import { MatchCore } from "./match";
 import { Pausable } from "./pausable";
 import { GenType } from "./genetype";
+import { Log } from "../../src/log";
 
 class DragonInfo {
     id         : u64;
@@ -42,6 +43,14 @@ class SeedInfo {
     seed2: u64;
 }
 
+class Addition {
+    addi: u64[];
+
+    constructor(info: u64[]) {
+        this.addi = info;
+    }
+}
+
 class FightContractData {
     // map betId => SeedInfo
     seeds         : Map<u64, SeedInfo>;
@@ -51,7 +60,7 @@ class FightContractData {
     skillDamageMin: u32[/*16*/];
     skillDamageMax: u32[/*16*/];
     offset: u64;
-    cAddition: Array<Array<u64>>;
+    cAddition: Addition[];
 
     constructor() {
         this.seeds          = new Map<u64, SeedInfo>();
@@ -61,9 +70,9 @@ class FightContractData {
         this.skillDamageMin = [15, 13, 18, 16, 21, 20, 23, 25, 30, 31, 33, 36, 41, 44, 47, 56];
         this.skillDamageMax = [15, 21, 19, 24, 22, 28, 29, 31, 32, 36, 41, 46, 47, 52, 59, 60];
         this.offset         = 100;
-        this.cAddition      = new Array<Array<u64>>();
-        this.cAddition[0]   =  [2523156596, 2523155320, 2690931308, 2355385712, 2858704744, 2355384440, 2523155324, 2523159156, 2355385716, 2858704752, 2690928752, 2523155312, 2271502184, 2355385708, 2523159144, 1350067340, 1266508940, 1517184144, 1433625732, 1600742536, 1350395012, 1266836620, 1517511820, 1433953412, 1517839508, 1349411988, 1265853580, 1432970388, 1433298052, 1516856460];
-        this.cAddition[1]   =  [1687582804, 1687584080, 1687907932, 1686926168, 1688234336, 1686602324, 1687585360, 1687582812, 1687255128, 1688564572, 1688236884, 1687581520, 1687906648, 1687252568, 1687582800, 2018547300, 2102104420, 1851431780, 1934986340, 1767875940, 2102430820, 2186319460, 1935317860, 2018873700, 1935321700, 1851105380, 2018219620, 1767218020, 1850775140, 2017890660];
+        this.cAddition      = [];
+        this.cAddition.push(new Addition([2523156596, 2523155320, 2690931308, 2355385712, 2858704744, 2355384440, 2523155324, 2523159156, 2355385716, 2858704752, 2690928752, 2523155312, 2271502184, 2355385708, 2523159144, 1350067340, 1266508940, 1517184144, 1433625732, 1600742536, 1350395012, 1266836620, 1517511820, 1433953412, 1517839508, 1349411988, 1265853580, 1432970388, 1433298052, 1516856460]));
+        this.cAddition.push(new Addition([1687582804, 1687584080, 1687907932, 1686926168, 1688234336, 1686602324, 1687585360, 1687582812, 1687255128, 1688564572, 1688236884, 1687581520, 1687906648, 1687252568, 1687582800, 2018547300, 2102104420, 1851431780, 1934986340, 1767875940, 2102430820, 2186319460, 1935317860, 2018873700, 1935321700, 1851105380, 2018219620, 1767218020, 1850775140, 2017890660]));
     }
 
 }
@@ -73,9 +82,7 @@ export class FightCore/*  extends Pausable */ {
     // private match: MatchCore;
 
     constructor(/* owner: account_name, match: MatchCore */) {
-        // super();
-        // this.owner = owner;
-        // this.match = match;
+        this.dataes = new FightContractData();
     }
 
     private getBlood(gen: GenType): u64 {
@@ -97,27 +104,26 @@ export class FightCore/*  extends Pausable */ {
         let double: u64   = 0;
         let cid: u64      = 0;
         let addition: u64 = 0;
-
         let character: u32 = gen.schar;               // schar一共有16bit
         let append: u32    = <u32>(gen.color >> 60);
-
-        let _gen = character << 4 | append; // _gen现在有20bit
+        let _gen = (character << 4 | append); // _gen现在有20bit
         for (let i = 0; i < 4; i++) {
             cid = (_gen >> (15 - i * 5)) & 0x1F;
-
             if (cid > 0) {
                 let idx: i32 = <i32>(cid - 1);
                 if (i < 2) {
-                    addition = this.dataes.cAddition[0][idx];
+                    // Log.s("GG 1").flush();
+                    addition = this.dataes.cAddition[0].addi[idx];
+                    // Log.s("GG 2").flush();
                 } else {
-                    addition = this.dataes.cAddition[1][idx];
+                    addition = this.dataes.cAddition[1].addi[idx];
                 }
 
-                blood  = (addition >> 24) & 0xff;
-                crit   = (addition >> 16) & 0xff;
-                dodge  = (addition >> 8) & 0xff;
-                double = addition & 0xff;
-
+                blood  = ((addition >> 24) & 0xff);
+                crit   = ((addition >> 16) & 0xff);
+                dodge  = ((addition >> 8) & 0xff);
+                double = (addition & 0xff);
+                // Log.s("GG 3").flush();
                 dinfo.additionNum += 1;
                 dinfo.bloodAdd    += blood;
                 dinfo.crit        += crit;
@@ -125,7 +131,6 @@ export class FightCore/*  extends Pausable */ {
                 dinfo.double      += double;
             }
         }
-
         dinfo.blood = dinfo.blood + <u64>(dinfo.blood * dinfo.bloodAdd / 1000) - <u64>(dinfo.additionNum * this.dataes.offset * dinfo.blood / 1000);
     }
 
@@ -180,22 +185,23 @@ export class FightCore/*  extends Pausable */ {
         d1.attackIndex = d1.attackIndex % 5;
     }
 
-    /*@action*/public startFight(betid: u64, did1: DragonId, gen1: GenType, did2: DragonId, gen2: GenType, nonce: u64): DragonId {
+    public startFight(betid: u64, did1: DragonId, gen1: GenType, did2: DragonId, gen2: GenType, nonce: u64): DragonId {
         let d1 = new DragonInfo();
         d1.id = did1;
         d1.blood = this.getBlood(gen1);
         d1.skills = this.getSkills(gen1);
         d1.skillLevels = this.getSkillsLevel(gen1);
         d1.seed = Action.random_uint64(nonce);
-
+Log.s("SS 1").flush();
         let d2 = new DragonInfo();
         d2.id = did2;
         d2.blood = this.getBlood(gen2);
         d2.skills = this.getSkills(gen2);
         d2.skillLevels = this.getSkillsLevel(gen2);
         d2.seed = Action.random_uint64(nonce);
-
+        Log.s("SS 2").flush();
         this.getAddition(gen1, d1);
+        Log.s("SS 3").flush();
         this.getAddition(gen2, d2);
 
         let si = new SeedInfo();
